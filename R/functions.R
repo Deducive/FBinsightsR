2ccfc8eff723017f12abf7e316ea2fc96529d36f
nycrefugee <paulnewnes@gmail.com>
2018-02-13 12:56:17 +0000

    removed outbound clicks.

--- R/functions.R
+++ R/functions.R
@@ -31,7 +31,7 @@ fbins_ag <- function(start_date, until_date, report_level, fb_access_token, acco
                   access_token = fb_access_token,
                   time_range = time_range,
                   level= report_level,
-                  fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, clicks, unique_clicks, outbound_clicks, unique_outbound_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
+                  fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, clicks, unique_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
                   time_increment="1",
                   limit = "10000",
                   breakdowns = "age, gender"
@@ -76,7 +76,7 @@ fbins_summ <- function(start_date, until_date, report_level, time_increment, fb_
                   access_token = fb_access_token,
                   time_range = time_range,
                   level = report_level,
-                  fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, frequency, clicks, unique_clicks, outbound_clicks, unique_outbound_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
+                  fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, frequency, clicks, unique_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
                   time_increment=time_increment,
                   limit = "10000"
                 ),
