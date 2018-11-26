82b58ed4a9bdc0f1782d9f8f5764a8d3ceaaed5b
Paul Newnes <paulnewnes@gmail.com>
2018-11-26 22:24:01 +0000

    paging
    paging functions

--- R/functions.R
+++ R/functions.R
@@ -36,14 +36,27 @@ fbins_ag <- function(start_date, until_date, report_level, fb_access_token, acco
                     level= report_level,
                     fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, clicks, unique_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
                     time_increment="1",
-                    limit = "10000",
+                    limit = "7500",
                     breakdowns = "age, gender"
                     ),
                   encode = "json",
                   verbose()))
   #extract data and name
-  content_result[["paging"]] <- NULL
-  data.frame(content_result$data %>% reduce(bind_rows))
+  content_result_df <- data.frame(content_result$data %>% reduce(bind_rows))
+  
+  # Condition to detect the next page
+  if(exists("next", content_result$paging) == TRUE){
+    # Checking from the originally returned list
+    result <- fromJSON(content_result$paging$`next`)
+    result_df <- bind_rows(content_result_df, as.tibble(result$data))
+    
+    # Looping through subsequent returned pages
+    while(exists("next", result$paging) == TRUE){
+      result <- fromJSON(result$paging$`next`)
+      result_df <- bind_rows(result_df, as.tibble(result$data))
+    }
+  }
+  result_df <- result_df
 }
 
 ###### Function 2 - fbins_summ
@@ -83,13 +96,26 @@ fbins_summ <- function(start_date, until_date, report_level, time_increment, fb_
                   level = report_level,
                   fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, frequency, clicks, unique_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
                   time_increment=time_increment,
-                  limit = "10000"
+                  limit = "7500"
                 ),
                 encode = "json",
                 verbose()))
   #extract data and name
-  content_result[["paging"]] <- NULL
-  data.frame(content_result$data %>% reduce(bind_rows))
+  content_result_df <- data.frame(content_result$data %>% reduce(bind_rows))
+  
+  # Condition to detect the next page
+  if(exists("next", content_result$paging) == TRUE){
+    # Checking from the originally returned list
+    result <- fromJSON(content_result$paging$`next`)
+    result_df <- bind_rows(content_result_df, as.tibble(result$data))
+    
+    # Looping through subsequent returned pages
+    while(exists("next", result$paging) == TRUE){
+      result <- fromJSON(result$paging$`next`)
+      result_df <- bind_rows(result_df, as.tibble(result$data))
+    }
+  }
+  result_df <- result_df
 }
 
 ###### Function 3 - fbins_page
@@ -200,5 +226,91 @@ fbins_insta <- function(start_date, until_date, time_period, page_access_token, 
     select(-end_time) %>% 
     select(ncol(result_df), 1:ncol(result_df))
 }
+
+###### Function 5 - fbins_pxa
+
+#' Facebook Ads Pixel Insights.
+#' This returns, in a data frame, pixel actions such as conversions or adds-to-cart together with a complement of ads stats such as impressions and clicks.
+#' This is applies pagination through the API - so it may take a time to return the results.
+#' @param start_date The first full day to report, in the format "YYYY-MM-DD" .
+#' @param until_date The last full day to report, in the format "YYYY-MM-DD" .
+#' @param report_level One of "ad", "adset", "campaign" or "account" .
+#' @param fb_access_token This must be a valid access token with sufficient privileges. Visit the Facebook API Graph Explorer to acquire one.
+#' @param account This is the ad account, campaign, adset or ad ID to be queried.
+#' @keywords facebook insights api
+#' @export
+#' @examples
+#' fbins_pxa("start_date", "until_date", "report_level", "time_increment", "fb_access_token", "account")
+#' fbins_pxa("2017-01-20", "2017-01-22", "ad", "ABCDEFG1234567890ABCDEFG", "act_12345678")
+#' fbins_pxa("2017-01-20", "2017-01-22", "ad", "ABCDEFG1234567890ABCDEFG", "act_12345678")
+
+fbins_pxa <- function(start_date, until_date, report_level, fb_access_token, account){
+  fbins_rev <- function(start_date, until_date, report_level, fb_access_token, account){
+    #set variables
+    sstring <- paste0('"','since','"')
+    ustring <- paste0('"','until','"')
+    #paste together JSON string
+    range_content <- paste0(sstring,':','"',start_date,'"',',',ustring,':','"',until_date,'"')
+    time_range <- paste0("{",range_content,"}")
+    #paste together URL
+    api_version <- "v3.1"
+    url_stem <- "https://graph.facebook.com/"
+    URL <- paste0(url_stem, api_version, "/", account, "/insights")
+    
+    #call insights
+    content_result <- content(GET(
+      URL,
+      query = list(
+        access_token = fb_access_token,
+        time_range = time_range,
+        level= report_level,
+        fields = "campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, actions, clicks",
+        time_increment="1",
+        limit = "7500",
+        action_breakdowns = "action_type",
+        breakdowns = ""
+      ),
+      encode = "json",
+      verbose()))
+    
+  }
+  get_pix_actions <- function(content_result){
+    # Extract & create 
+    nested_df <- map_dfr(content_result$data, ~modify_at(.,"actions", compose(list,bind_rows)))
+    missing_actions <- lengths(nested_df$actions) == 0
+    nested_df$actions[missing_actions] <- replicate(sum(missing_actions),
+                                                    tibble(action_device = NA, action_type = NA, value = NA), F)
+    
+    # Filter for only pixel actions
+    unnest(nested_df) %>%
+      filter(action_type == "offsite_conversion.fb_pixel_add_to_cart" |
+               action_type == "offsite_conversion.fb_pixel_custom" |
+               action_type == "offsite_conversion.fb_pixel_purchase")
+  }
+  
+  content_result <- fbins_rev(start_date, until_date, report_level, fb_access_token, account)
+  result_df <- get_pix_actions(content_result)
+  
+  # result_df4 <- get_pix_actions(content_result)
+  
+  # Condition to detect the next page
+  if(exists("next", content_result$paging) == TRUE){
+    # GET from the nextURL if it exists
+    content_result2 <- content(GET(content_result$paging$`next`))
+    result2_df <- get_pix_actions(content_result2)
+    result_df <- bind_rows(result_df, result2_df)
+    
+    # Looping through subsequent returned pages
+    while(exists("next", content_result2$paging) == TRUE){
+      content_result2 <- content(GET(content_result2$paging$`next`))
+      result2_df <- get_pix_actions(content_result2)
+      result_df <- bind_rows(result_df, result2_df)
+    }
+  }
+  result_df <- result_df
+}
+
+
+
 
 
