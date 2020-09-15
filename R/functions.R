ea763247be387658dcdaf4632d8dd62b165da0f2
Paul Newnes <paulnewnes@gmail.com>
2020-09-15 19:15:33 +0100

    updated to API v8.0

--- R/functions.R
+++ R/functions.R
@@ -23,7 +23,7 @@ fbins_ag <- function(start_date, until_date, report_level, fb_access_token, acco
   range_content <- paste0(sstring,':','"',start_date,'"',',',ustring,':','"',until_date,'"')
   time_range <- paste0("{",range_content,"}")
   #paste together URL
-  api_version <- "v5.0"
+  api_version <- "v8.0"
   url_stem <- "https://graph.facebook.com/"
   URL <- paste0(url_stem, api_version, "/", account, "/insights")
   
@@ -96,7 +96,7 @@ fbins_summ <- function(start_date, until_date, report_level, time_increment, fb_
   range_content <- paste0(sstring,':','"',start_date,'"',',',ustring,':','"',until_date,'"')
   time_range <- paste0("{",range_content,"}")
   #paste together URL
-  api_version <- "v5.0"
+  api_version <- "v8.0"
   url_stem <- "https://graph.facebook.com/"
   URL <- paste0(url_stem, api_version, "/", account, "/insights")
   
@@ -147,7 +147,7 @@ fbins_summ <- function(start_date, until_date, report_level, time_increment, fb_
 
 fbins_page <- function(start_date, until_date, time_period, page_access_token, page_account){
   #paste together URL
-  api_version <- "v5.0"
+  api_version <- "v8.0"
   url_stem <- "https://graph.facebook.com/"
   URL <- paste0(url_stem, api_version, "/", page_account, "/insights")
   
@@ -202,7 +202,7 @@ fbins_page <- function(start_date, until_date, time_period, page_access_token, p
 
 fbins_insta <- function(start_date, until_date, time_period, page_access_token, insta_account){
   #paste together URL
-  api_version <- "v5.0"
+  api_version <- "v8.0"
   url_stem <- "https://graph.facebook.com/"
   URL <- paste0(url_stem, api_version, "/", insta_account, "/insights")
   
@@ -265,7 +265,7 @@ fbins_pxa <- function(start_date, until_date, report_level, fb_access_token, acc
     range_content <- paste0(sstring,':','"',start_date,'"',',',ustring,':','"',until_date,'"')
     time_range <- paste0("{",range_content,"}")
     #paste together URL
-    api_version <- "v5.0"
+    api_version <- "v8.0"
     url_stem <- "https://graph.facebook.com/"
     URL <- paste0(url_stem, api_version, "/", account, "/insights")
     
